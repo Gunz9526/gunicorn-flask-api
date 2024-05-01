cf32162044edef75212d9efb2fbaa272394d6db2
Gunz9526 <kkw9526@gmail.com>
2024-05-01 21:01:50 +0900

    edit controller and model

---
+++ controller/con_member.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def login():
+    pass
+
+def join_memeber():
+    pass
+
+def find_password():
+    pass
+
+def edit_info():
+    pass
+
+def discard_info():
+    pass
---
+++ model/model_member.py
@@ -0,0 +1,22 @@
+from sqlalchemy import *
+from flask_jwt_extended import jwt_manager, jwt_required
+def check_password():
+    pass
+
+def check_tokens():
+    pass
+
+def find_password():
+    pass
+
+def join_members():
+    pass
+
+def check_login():
+    pass
+
+def edit_member_info():
+    pass
+
+def discard_member_info():
+    pass
